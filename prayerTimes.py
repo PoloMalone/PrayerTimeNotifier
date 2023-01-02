5767017345959190a26137dfcd793586df077bfe
Josef <57298459+PoloMalone@users.noreply.github.com>
2023-01-02 15:43:03 +0100

    Clean up

--- prayerTimes.py
+++ prayerTimes.py
@@ -1,7 +1,7 @@
 import datetime
 import pytz
 import requests
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import json
 from timezonefinder import TimezoneFinder
@@ -11,42 +11,28 @@ from win10toast import ToastNotifier
 import os
 
 
+#toast notifier
 def notif(pray_name):
     n.show_toast("PrayerTimes", pray_name + " comes in " + str(notify_me) +  " minutes", duration = None, threaded=True,
     icon_path = os.getcwd() + "/Prayerbeads.ico")
 
-
-
+#initialize
 def init():
     prayer_times_dict, refined_time = get_new_times()
     pName, pTime, index = check_each_prayer(prayer_times_dict,refined_time)
-    timeleft = get_notification_time(pName,pTime,index)
+    timeleft = get_notification_time(pTime,index)
     sleep_until_notif_time(timeleft,pName,index)
-    root.mainloop()
 
-def prayer_times_today(prayer_times_dict):
+#update the new times from response to labels
+def update_prayer_times_today(prayer_times_dict):
+    today_prayer_time_FAJR.config(text ="Fajr: " + prayer_times_dict["Fajr"])
+    today_prayer_time_SUNRISE.config(text = "Shuruq: " + prayer_times_dict["Sunrise"])
+    today_prayer_time_DHUHR.config(text ="Dhuhr: " + prayer_times_dict["Dhuhr"])
+    today_prayer_time_ASR.config(text ="Asr: " + prayer_times_dict["Asr"])
+    today_prayer_time_MAGHRIB.config(text ="Maghrib: " + prayer_times_dict["Maghrib"])
+    today_prayer_time_ISHA.config(text ="Isha: " + prayer_times_dict["Isha"])
 
-    today_prayer_time_FAJR = Label(root, text ="Fajr: " + prayer_times_dict["Fajr"])
-    today_prayer_time_SUNRISE = Label(root, text = "Shuruq: " + prayer_times_dict["Sunrise"])
-    today_prayer_time_DHUHR = Label(root, text ="Dhuhr: " + prayer_times_dict["Dhuhr"])
-    today_prayer_time_ASR = Label(root, text ="Asr: " + prayer_times_dict["Asr"])
-    today_prayer_time_MAGHRIB = Label(root, text ="Maghrib: " + prayer_times_dict["Maghrib"])
-    today_prayer_time_ISHA = Label(root, text ="Isha: " + prayer_times_dict["Isha"])
-
-    labels.append(today_prayer_time_FAJR)
-    labels.append(today_prayer_time_SUNRISE)
-    labels.append(today_prayer_time_DHUHR)
-    labels.append(today_prayer_time_ASR)
-    labels.append(today_prayer_time_MAGHRIB)
-    labels.append(today_prayer_time_ISHA)
-    
-    today_prayer_time_FAJR.pack()
-    today_prayer_time_SUNRISE.pack()
-    today_prayer_time_DHUHR.pack()
-    today_prayer_time_ASR.pack()
-    today_prayer_time_MAGHRIB.pack()
-    today_prayer_time_ISHA.pack()
-
+#get time now depending on timezone
 def time_now_live(time):
     time_now = datetime.datetime.now(timezone)
     time_now_refined = time_now.replace(tzinfo=None)
@@ -55,39 +41,26 @@ def time_now_live(time):
     time.config(text=" Time Now: " + str(time_now_refined), fg='RED')
     return
 
-
+#loop until new day
 def sleep_until_new_day():
-    rem_time_twelve = Label(root, text = " Timeleft bofore 00:00")
-    labels.append(rem_time_twelve)
-    rem_time_twelve.pack()
-    label_time_now = Label(root, text= "Time now: ")
-    labels.append(label_time_now)
-    label_time_now.pack()
     deadline = "00:00:00"
     deadline_format = datetime.datetime.strptime(deadline, '%H:%M:%S')
     while True:
         time_now = datetime.datetime.now(timezone).replace(tzinfo=None)
         remaining_time_twelve = str((deadline_format - time_now).total_seconds()).split(".")[0]
         remaining_time_twelve = int(remaining_time_twelve)
-        rem_time_twelve.config(text=" Timeleft before 00:00 - " + convert(remaining_time_twelve))
+        rem_time.config(text=" Timeleft before 00:00 - " + convert(remaining_time_twelve))
         time_now_live(label_time_now)
         root.update()
         sleep(0.1)
         stringed_time_now = time_now.strftime('%H:%M:%S')
         if stringed_time_now == deadline:
-            for label in labels: label.destroy()
             break;
     init()
     return  
 
-
+#loop until notif time
 def sleep_until_notif_time(notif_time, pray_name, idx):
-    rem_time = Label(root, text=" Timeleft before notif time ")
-    label_time_now = Label(root, text= "Time now: ")
-    labels.append(rem_time)
-    labels.append(label_time_now)
-    rem_time.pack()
-    label_time_now.pack()
     if pray_name == "Sunrise":
         pray_name = "Shuruq"
     while notif_time > datetime.datetime.now(timezone).replace(tzinfo=None):
@@ -99,28 +72,24 @@ def sleep_until_notif_time(notif_time, pray_name, idx):
         root.update()
         sleep(0.1)
     notif(pray_name)
-    for label in labels: label.destroy()
     if idx == 5:
         sleep_until_new_day()
     else:
-        print("INNE ELSEEEEEE")
         init()
     return
 
+#convert from seconds to Hours, minutes, seconds
 def convert(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
     seconds %= 60
-     
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
-
-def get_notification_time(prayer_name, prayer_time, index):
-    # Calculate the minutes before the prayer
+#get notif time depending on user input
+def get_notification_time(prayer_time, index):
     midnight = datetime.datetime.now(timezone).replace(hour=0, minute=0, second=0, microsecond=0).replace(tzinfo=None)
-
     if index == 0 and datetime.datetime.now(timezone).replace(tzinfo=None) < midnight :
         notification_time = datetime.datetime.combine(
             datetime.datetime.now(timezone).replace(tzinfo=None), prayer_time 
@@ -130,27 +99,13 @@ def get_notification_time(prayer_name, prayer_time, index):
         notification_time = datetime.datetime.combine(
             datetime.datetime.now(timezone).replace(tzinfo=None), prayer_time
         ) - datetime.timedelta(minutes=notify_me)
-        print("Prayer: " + str(prayer_name))
-        print(" ")
-        print("Notif time: "+ str(notification_time))
-        # Get the current time in the specified timezone
-        time_now = datetime.datetime.now(timezone).replace(tzinfo=None)
-        print("Timenow: " + str(time_now))
-        print(" ")
     
         return notification_time
 
-
-
-
+#http request to get prayer times depending on timezone and location
 def get_new_times():
     current_time = datetime.datetime.now(timezone).replace(tzinfo=None) 
-    print("CURRENT DATE: "+ str(current_time))
-    print(" ")
     current_date = datetime.datetime.strftime(current_time, '%d-%m-%Y')
-    print("refined DATE: " + str(current_date))
-    print(" ")
-
     current_day = int(current_date.split("-")[0])
     current_month = int(current_date.split("-")[1])
     current_year = int(current_date.split("-")[2])
@@ -161,7 +116,6 @@ def get_new_times():
 
     current_time_split = str(current_time).split(" ")
     refined_current_time = current_time_split[1].split(".")[0]
-    print("refined TIME: " + str(refined_current_time))
 
     # Send a request to the website to get the prayer times
     response = requests.get(prayer_times_url)
@@ -169,6 +123,7 @@ def get_new_times():
     # Parse the response to get the prayer times
     prayer_times = response.json()
 
+    #insert values to dict
     prayer_times_dict["Fajr"] = (prayer_times["data"][current_day-1]["timings"]["Fajr"].split()[0])
     prayer_times_dict["Sunrise"] = (prayer_times["data"][current_day-1]["timings"]["Sunrise"].split()[0])
     prayer_times_dict["Dhuhr"] = (prayer_times["data"][current_day-1]["timings"]["Dhuhr"].split()[0])
@@ -176,46 +131,33 @@ def get_new_times():
     prayer_times_dict["Maghrib"] = (prayer_times["data"][current_day-1]["timings"]["Maghrib"].split()[0])
     prayer_times_dict["Isha"] = (prayer_times["data"][current_day-1]["timings"]["Isha"].split()[0])
     
-    # The names of the Islamic prayers
-    print(prayer_times_dict)
-    print(refined_current_time)
-    prayer_times_today(prayer_times_dict)
+    #update labels
+    update_prayer_times_today(prayer_times_dict)
     return prayer_times_dict, refined_current_time
 
+#check what prayer is next
 def check_each_prayer(prayer_times_dict, ref_currtime):
-  
     for index, prayer_name in enumerate(prayer_times_dict):
-        
-        # Get the prayer time from the response
         prayer_time = prayer_times_dict[prayer_name]
-        print(prayer_name)
-
         prayer_time = datetime.time.fromisoformat(prayer_time)
-        print(prayer_time)
 
         currtime_totime = datetime.datetime.strptime(ref_currtime, '%H:%M:%S').time()
         currtime_todate = datetime.datetime.combine(
-            datetime.date.today(), currtime_totime
-        )
-        print(currtime_todate)
+            datetime.date.today(), currtime_totime)
         prayertime_todate = datetime.datetime.combine(
-            datetime.date.today(), prayer_time
-        )
-        print(prayertime_todate)
+            datetime.date.today(), prayer_time)
+
         diff = prayertime_todate - currtime_todate
   
         if not str(diff).startswith('-') and int(diff.total_seconds()) > (notify_me*60):
-            print(prayer_name)
-            print(prayer_time)
-            print(index)
             return prayer_name, prayer_time, index
         if index == 5:
             sleep_until_new_day()
             return
 
-      
-
+#when user clicks "save" update values and call init
 def save_inputs():
+    global data
     global timezone
     global latitude
     global longitude
@@ -230,7 +172,6 @@ def save_inputs():
         time_to_notif_label.config(text="Input integer please")
         return
 
-
     for i in range(len(data)):
         if data[i]['name'] == city:
             latitude = float(data[i]['lat'])
@@ -240,31 +181,59 @@ def save_inputs():
     timezone_result = obj.timezone_at(lat=latitude, lng=longitude)
     timezone = pytz.timezone(timezone_result) 
     method = methods_dict[method]
-    for label in labels: label.destroy()
     init()
     return
 
+#destroy window when exiting
+def on_closing():
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        root.destroy()
+       
+#pack all labels
+def pack_labels():
+    method_label.pack()
+    choose_method.pack()
+    location_label.pack()
+    choose_city.pack()
+    time_to_notif_label.pack()
+    choose_time_to_notif.pack()
+    save_button.pack()
+    today_prayer_time_FAJR.pack()
+    today_prayer_time_SUNRISE.pack()
+    today_prayer_time_DHUHR.pack()
+    today_prayer_time_ASR.pack()
+    today_prayer_time_MAGHRIB.pack()
+    today_prayer_time_ISHA.pack()
+    rem_time.pack()
+    label_time_now.pack()
+    signature.place(relx = 0.0,
+                    rely = 1.0,
+                    anchor ='sw')
+    version.place(relx = 1.0,
+                    rely = 1.0,
+                    anchor ='se')
 
- 
 
 if __name__ == "__main__":
     
-    global data
-  
-    root = Tk()
+    #init values for window
+    root = tk.Tk()
     n = ToastNotifier()
-    img = PhotoImage(file=os.getcwd() + "/Prayerbeads.png")
+    img = tk.PhotoImage(file=os.getcwd() + "/Prayerbeads.png")
     root.geometry("300x400")
     root.title("Prayer Times")
     root.wm_iconphoto(True, img)
-    
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+
+    #init values
     timezone = pytz.timezone('Europe/Stockholm') 
     method = "3"
     longitude = "18.063240"
     latitude = "59.334591"
     notify_me = 10
+    cities = []
 
-    labels = []
+    #different calculating methods to showcase in spinbox
     methods = ['University of Islamic Sciences, Karachi', #1
                 'Islamic Society of North America', #2
                 'Muslim World League', #3
@@ -280,8 +249,7 @@ if __name__ == "__main__":
                 'Spiritual Administration of Muslims of Russia', #14
                 'Moonsighting Committee Worldwide (also requires shafaq paramteer)'] #15
 
-    cities = []
-
+    #different calculating methods with keys and values to use for http request
     methods_dict = {
                 'University of Islamic Sciences, Karachi': 1,
                 'Islamic Society of North America': 2,
@@ -299,22 +267,21 @@ if __name__ == "__main__":
                 'Moonsighting Committee Worldwide (also requires shafaq paramteer)': 15
     }
 
-
+    #get json data
     data_file =  open('cities.json', encoding="utf8")   
     data = json.load(data_file)
     for v in data:
         cities.append(v["name"])
     data_file.close()
 
-
-
-    field_method = StringVar(root)
-    field_location = StringVar(root)
-    field_time_to_notif = StringVar(root)
+    #Labels definitions
+    field_method = tk.StringVar(root)
+    field_location = tk.StringVar(root)
+    field_time_to_notif = tk.StringVar(root)
     field_method.set('Muslim World League')
     field_location.set('Stockholm')
     field_time_to_notif.set('10')
-    method_label = Label(root, text ="Calculating method: ")
+    method_label = tk.Label(root, text ="Calculating method: ")
     choose_method = AutocompleteCombobox(
         root, 
         width=30, 
@@ -322,9 +289,8 @@ if __name__ == "__main__":
         completevalues=methods,
         textvariable=field_method
         )
-    method_label.pack()
-    choose_method.pack()
-    location_label = Label(root, text ="Location(city): ")
+
+    location_label = tk.Label(root, text ="Location(city): ")
     choose_city = AutocompleteCombobox(
         root, 
         width=30, 
@@ -332,30 +298,35 @@ if __name__ == "__main__":
         completevalues=cities,
         textvariable=field_location
         )
-    location_label.pack()
-    choose_city.pack()
-
-    time_to_notif_label = Label(root, text ="Notify me minutes before prayer: ")
-    choose_time_to_notif = Entry(
+    
+    time_to_notif_label = tk.Label(root, text ="Notify me minutes before prayer: ")
+    choose_time_to_notif = tk.Entry(
         root, 
         width=5, 
         font=('Times', 15),
         textvariable=field_time_to_notif
         )
 
-    time_to_notif_label.pack()
-    choose_time_to_notif.pack()
-
-
-    save_button = Button(root,
+    save_button = tk.Button(root,
                             text = "Save", 
                             command = save_inputs)
 
 
-    save_button.pack()
-    
-    
+    today_prayer_time_FAJR = tk.Label(root, text ="")
+    today_prayer_time_SUNRISE = tk.Label(root, text = "")
+    today_prayer_time_DHUHR = tk.Label(root, text ="")
+    today_prayer_time_ASR = tk.Label(root, text ="")
+    today_prayer_time_MAGHRIB = tk.Label(root, text ="")
+    today_prayer_time_ISHA = tk.Label(root, text ="")
+
+    rem_time = tk.Label(root, text="")
+    label_time_now = tk.Label(root, text= "")
+  
+    signature = tk.Label(root, text="Made by Polo 2023-01-01", font=('Times', 8))
+    version = tk.Label(root, text="v 1.2.0.3",font=('Times', 8))
+
+    pack_labels()
     init()
-    
+    root.mainloop()
 
-
+   
